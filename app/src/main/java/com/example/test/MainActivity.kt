61eb2dfdc3cc6d16ef27ecd592898e39626175f2
student <s1082823@gm.pu.edu.tw>
2021-03-19 15:50:40 +0800

    buttom的onClick事件

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener{
+
+    var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        btn.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        TODO("Not yet implemented")
+        if (view.id.equals(R.id.txv)){
+            counter++
+        }
+        else{
+            counter = 0
+        }
+
+        txv.text = counter.toString()
+    }
+}
